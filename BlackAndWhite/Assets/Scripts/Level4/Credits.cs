dfafa93e67b9bf575fb1fb664d962400766ab2f5
blueloveTH <blueloveTH@qq.com>
2021-04-23 22:44:44 +0800

    UP

---
+++ BlackAndWhite/Assets/Scripts/Level4/Credits.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] float speed = 100;
+
+    private void Start()
+    {
+        var txts = GetComponentsInChildren<TextMeshProUGUI>();
+        string s = "";
+        foreach (var item in txts)
+        {
+            s += item.text + "\n";
+        }
+        print(s);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.up * speed * Time.deltaTime;
+        if (transform.localPosition.y > 1400)
+            transform.localPosition = new Vector3(0, -285, 0);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+}
--- BlackAndWhite/Assets/Scripts/Level4/FinalTest.cs
+++ BlackAndWhite/Assets/Scripts/Level4/FinalTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using GameFlow;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class FinalTest : InteractiveBehaviour
 {
@@ -24,5 +25,15 @@ public class FinalTest : InteractiveBehaviour
         btnCount++;
         if (btnCount == 3)
             circleTeleport.Unlock();
+    }
+
+    public void Win()
+    {
+        Camera.main.GetComponent<CameraFollow>().enabled = false;
+
+        var img = GameObject.Find("Canvas/UI Mask").GetComponent<UnityEngine.UI.Image>();
+        img.DOFade(1, 1f).SetEase(Ease.OutQuad).OnComplete(
+            () => SceneManager.LoadScene("The End")
+            );
     }
 }
